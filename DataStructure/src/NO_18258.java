aa0fd5dc0af5678c1bf483095b3eda7c0da7792f
jji3379 <jji3379@naver.com>
2025-05-10 06:21:08 +0900

    NO_18258 큐 2

--- DataStructure/src/NO_18258.java
+++ DataStructure/src/NO_18258.java
@@ -1,5 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class NO_18258 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        Deque deque = new ArrayDeque();
+
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+
+            if (line.contains("push")) {
+                deque.offer(line.replace("push ", ""));
+            }
+
+            switch (line) {
+                case "pop":
+                    if (deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.pop()).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(deque.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(deque.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "front":
+                    if (deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.peek()).append("\n");
+                    }
+                    break;
+                case "back":
+                    if (deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.getLast()).append("\n");
+                    }
+                    break;
+            }
+        }
+
+        System.out.println(sb);
     }
 }
