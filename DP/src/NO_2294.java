3d8dff9b760c95342ef9bbfb0a220d1bad5e58f2
jji3379 <jji3379@naver.com>
2025-10-04 22:36:00 +0900

    feat: NO_2294 동전 2

---
+++ DP/src/NO_2294.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/2294
+ */
+public class NO_2294 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(coins);
+
+        int[] dp = new int[k + 1];
+        Arrays.fill(dp, 100001);
+        dp[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = coins[i]; j <= k; j++) {
+                dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+            }
+        }
+
+        if (dp[k] == 100001) {
+            System.out.print(-1);
+        } else {
+            System.out.print(dp[k]);
+        }
+    }
+}
